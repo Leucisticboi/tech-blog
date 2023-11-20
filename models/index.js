49652559645cf962d5cb1e980c42c9b8b5629b03
Leucisticboi <116482248+Leucisticboi@users.noreply.github.com>
2023-11-20 12:32:26 -0700

    Updated models/index.js paths

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
-const User = require('./user');
-const Post = require('./post');
-const Comment = require('./comment');
+const User = require('./user.js');
+const Post = require('./post.js');
+const Comment = require('./comment.js');
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
