089f3b9d8ead9fd6169c03f719744e8942736cb6
Leucisticboi <116482248+Leucisticboi@users.noreply.github.com>
2023-11-20 12:07:32 -0700

    Updated connection.js && .env

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
+  ? new Sequelize(process.env.JAWSDB_URL, process.env.JAWSDB_USER, process.env.JAWSDB_PW)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
