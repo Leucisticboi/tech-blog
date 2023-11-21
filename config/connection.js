60b928fee410fd7336d0a13c61e9c67679f594af
Leucisticboi <116482248+Leucisticboi@users.noreply.github.com>
2023-11-21 04:24:40 -0700

    Hmm

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,9 @@
-const Sequelize = require('sequelize');
-require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  // If JAWSDB_URL is set, use that for the connection
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  // Use separate environment variables
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-      port: process.env.DB_PORT
-    }
-  );
-}
-
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'mysql'
+});
 
 module.exports = sequelize;
